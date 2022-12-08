662395f1f3f02f9f55d688d65e8dfd24f773502b
pablobrescia <118524208+pablobrescia@users.noreply.github.com>
2022-12-08 18:01:45 +0100

    Add files via upload

--- js/javazenaida.js
+++ js/javazenaida.js
@@ -2,7 +2,7 @@
 /*  SCRIPTS de Pedagogía Zenaida                                               */                                
 /*  Autor: Pablo Brescia Rueda                                                 */
 /*  Versión: 1.0                                                               */
-/*  21 de octubre de 2022                                                      */
+/*  02/12/2022                                                                 */
 /*******************************************************************************/
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
